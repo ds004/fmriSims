7e30ec7866fc455c32e3a2730ab6fbb94e56a7af
Dave Dell <dgs242@cornell.edu>
2016-04-08 12:46:45 -0400

    realFile

--- simsCode.R
+++ simsCode.R
@@ -1,5 +1,19 @@
-#this is going ot be my simulation file GUH
-#let's see!!!!!
-#cats!!!!
+#alright, simulations for the fMRI thing
 
-not helpful at all
+#so important simulations. comparing ravikumar on graphs with no misclassification
+#and graphs with misclassification.
+
+#step 1, create networks... scale free?
+#step 2, generate Ising Data with those networks
+#step 3, fit IsingFit on those networks!
+
+#Okay, cool. 
+
+#Step 1. can do Erdos Renyi Model and scale-free. It is possibe that scale-free is hard
+
+#scalefree network simulation: http://gdangelo.web.cs.unibo.it/pool/ricerca/simutools2009/gda-simutools-2009.pdf
+
+#here might be better, in terms of goiing fast. the above link is too general
+#http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=5349196
+
+#f[i] denotes an indicator of the node v[i] in the case of adding a new node
