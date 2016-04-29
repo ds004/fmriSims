63a19d6d93e90b3e7d357c917d1ba2b60ed05c50
DaveMBA <dgs242@cornell.edu>
2016-04-29 18:06:45 -0400

    Apr29 updates

--- EMmeth.R
+++ EMmeth.R
@@ -4,3 +4,11 @@
 #Step 1. Small groups or something. small amount of nodes. maybe 1 misclassified edge. 
 
 #Step 2. Write out the Q function, and how to maximize it. 
+
+
+Okay
+raptors at 7:30
+blue jays at 7:10
+blues at 8:00
+#nothing 'til then
+
