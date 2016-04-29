a3ed438e51ca9e97b64d35d6efff84677441d7c1
DaveMBA <dgs242@cornell.edu>
2016-04-29 13:56:10 -0400

    Apr29 updates

---
+++ EMmeth.R
@@ -0,0 +1,6 @@
+#EM algorithm on areas of the graph
+
+
+#Step 1. Small groups or something. small amount of nodes. maybe 1 misclassified edge. 
+
+#Step 2. Write out the Q function, and how to maximize it. 
