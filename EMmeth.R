870abbd981a46135070dac136130954049f60116
Dave Dell <dgs242@cornell.edu>
2016-04-30 15:45:02 -0400

    so mad

--- EMmeth.R
+++ EMmeth.R
@@ -6,9 +6,21 @@
 #Step 2. Write out the Q function, and how to maximize it. 
 
 
-Okay
-raptors at 7:30
-blue jays at 7:10
-blues at 8:00
-#nothing 'til then
+#Okay, so let's see. 
 
+#Generate true network
+#Use ScaFre from simsCode.R
+
+trueNet = ScaFre(50)
+
+#okay now using that, generate some Ising data. 
+#Use IsingSampler function from simsCode.R
+
+nsim = 100
+thresholds = rep(0, 50)
+sim = IsingSampler(nsim, trueNet, thresholds, responses = c(-1L, 1L)) == TRUE
+
+#misclassify 3 connected nodes 10% of the time. 
+
+
+#initial fit
