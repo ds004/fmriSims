455cc5902ada897253278600b38f27d1491479eb
DaveMBA <dgs242@cornell.edu>
2016-05-01 01:43:37 -0400

    May1 1:43AM updates

--- EMmeth.R
+++ EMmeth.R
@@ -12,15 +12,100 @@
 #Use ScaFre from simsCode.R
 
 trueNet = ScaFre(50)
-
+#save(trueNet, file = 'trueNet')
+graph = graph.adjacency(trueNet, mode = "undirected")
+plot(graph, vertex.size = 3, vertex.label = NA)
 #okay now using that, generate some Ising data. 
 #Use IsingSampler function from simsCode.R
 
-nsim = 100
+nsim = 200
 thresholds = rep(0, 50)
 sim = IsingSampler(nsim, trueNet, thresholds, responses = c(-1L, 1L)) == TRUE
-
 #misclassify 3 connected nodes 10% of the time. 
+#choose 3 nodes that are somewhat highly connected. 
+#choose nodes 16, 30, and 44
+MISS = c(16, 30, 44)
+msim = sim
+m16 = sample(1:200,20); m30 = sample(1:200, 20); m44 = sample(1:200, 20)
+msim[m16,16]= !msim[m16, 16]
+msim[m30, 30] = !msim[m30,30]
+msim[m44, 44] = !msim[m44, 44] 
 
+#the neighborhoods we're interested in are
+neighborhood(graph, order = 1, nodes = c(16, 30, 44))
 
 #initial fit
+Fit = IsingFit(msim)
+
+#save(Fit, file = "Fit")
+fitAdj = Fit$weiadj != 0
+graphFit = graph.adjacency(fitAdj, mode = "undirected")
+
+#true M^N is 
+tMN = unique(unlist(neighborhood(graph, order = 2, nodes = c(16, 30, 44))))
+#fittedM^N is
+fMN = unique(unlist(neighborhood(graphFit, order = 2, nodes = c(16, 30, 44))))
+
+#label graphFit
+colnames(fitAdj) = 1:50
+rownames(fitAdj) = 1:50
+
+edges = get.edgelist(graphFit)
+newEdge = matrix(NA, ncol = 2)[-1,]
+for (i in 1:dim(edges)[1]) {
+	if ((edges[i,1] %in% fMN) | (edges[i,2] %in% fMN)) newEdge = rbind(newEdge, edges[i,])
+}
+
+subNet = graph_from_edgelist(newEdge, directed = FALSE)
+K = max(c(newEdge))
+if (K < 50) add_vertices(subNet, 50-K)
+subAdj = get.adjacency(subNet)
+
+#the lambda we use is 
+lam = mean(Fit$lambda.values[fMN])
+
+#now for each node, we need to calculate all the weights
+
+#need to do this given weights
+
+#we update these parameters in an outer loop.
+edgePar = Fit$weiadj
+nodePar = Fit$thresholds
+CLASS = fMN[which(!(fMN %in% MISS))]
+
+for (n = 1:200) {
+#calculate the partition function for the conditional
+#start partFn and z at 1 since when all obs are 0, A(z_c, z_m) = 1
+	partFn = 1
+	obs = msim[n,]
+	for (z in 1:(2^3-1)) {
+		zm = as.numeric(intToBits(z))[1:3]
+		partFn = partFn + assocFunc(zm, MISS, CLASS, edgePar, nodePar, obs)
+	}
+}
+
+
+
+
+#Given misclassified z nodes, their vertex numbers, vertex numbers for classified nodes, edge parameters, and node parameters, calculate A(z_c, z_m)
+assocFunc <- function(z_m, MISS, CLASS, edge, node, obs) {
+	p1 = z_m%*%node[MISS]
+	p2 = 0
+	for (m in 1:length(MISS)) {
+		lnodes = c(MISS, CLASS)[which(c(MISS, CLASS) > MISS[m])]
+		if (length(lnodes) > 0)p2 = p2 + z_m[m]*edge[m,lnodes]%*%obs[lnodes]
+	}
+	exp(p1 + p2)
+}
+
+
+
+
+
+
+
+
+
+
+
+
